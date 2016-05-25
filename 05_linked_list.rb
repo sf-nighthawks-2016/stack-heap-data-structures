a1b71c7d590015e76a7733042898e3874f59e586
Sarah Spikes <sdspikes@cs.stanford.edu>
2016-05-25 10:27:26 -0700

    two options for iterating through a linked list

--- 05_linked_list.rb
+++ 05_linked_list.rb
@@ -10,8 +10,21 @@ end
 
 linked_list = Node.new(1, Node.new(2, Node.new(10)))
 
-node = linked_list
-while node != nil
-  p node.value
-  node = node.next
+# The standard idiom for iterating through a linked list
+def iterate(node)
+  while node != nil
+    p node.value
+    node = node.next
+  end
 end
+
+# The recursive way to iterate through a linked list
+def iterate_recursive(node)
+  return if !node
+  p node.value
+  iterate_recursive(node.next)
+end
+
+# iterate(linked_list)
+
+iterate_recursive(linked_list)
